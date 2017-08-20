5dc5e24e862a5e3592443b77c701b867fe66e9a9
Zach Calvert <ZachCalvert@gmail.com>
2017-08-20 17:55:31 -0500

    Initial project

---
+++ src/main/java/com/zachcalvert/PictureScript/PictureScriptApplication.java
@@ -0,0 +1,12 @@
+package com.zachcalvert.PictureScript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureScriptApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zachcalvert/PictureScript/PictureScriptApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zachcalvert.PictureScript;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PictureScriptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
