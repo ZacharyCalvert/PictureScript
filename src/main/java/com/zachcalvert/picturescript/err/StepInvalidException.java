1c9fe1f3adc7734cb92da627d1bc165e2339ae88
Zach Calvert <ZachCalvert@gmail.com>
2017-09-05 21:21:17 -0500

    Adding step errors

---
+++ src/main/java/com/zachcalvert/picturescript/err/StepInvalidException.java
@@ -0,0 +1,8 @@
+package com.zachcalvert.picturescript.err;
+
+public class StepInvalidException extends RuntimeException {
+
+  public StepInvalidException(String message) {
+    super(message);
+  }
+}
