9b1b2dd6c268ada6d32b567b34ec26b3eb345675
Zach Calvert <ZachCalvert@gmail.com>
2017-09-07 21:11:38 -0500

    Adding repository helper functions

--- src/main/java/com/zachcalvert/picturescript/repository/FileRepository.java
+++ src/main/java/com/zachcalvert/picturescript/repository/FileRepository.java
@@ -1,6 +1,7 @@
 package com.zachcalvert.picturescript.repository;
 
 import com.zachcalvert.picturescript.model.File;
+import com.zachcalvert.picturescript.model.FolderBase;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ public interface FileRepository extends JpaRepository<File, Long> {
 
   @Query("SELECT DISTINCT f.sha256 FROM File f WHERE f.extension in :fileTypes and f.folderBase.fromOutput = false")
   List<String> findRequiredOutputShaSums(@Param("fileTypes") List<String> fileTypes);
+
+  @Query("SELECT DISTINCT f.sha256 FROM File f WHERE f.sha256 in :shaSums and f.folderBase <> :folderBase")
+  List<String> findMoveShaSums(@Param("shaSums") List<String> shaSums, @Param("folderBase") FolderBase folderBase);
 }
